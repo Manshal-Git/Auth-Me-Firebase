7fa9b16d19ce41d43b1db3ddb21a01d6e9643b3f
Manshal Khatri <khatrimanshal1206@gmail.com>
2022-07-17 12:26:28 +0530

    Added Plugin and Dependencies for Firebase

--- app/src/main/java/com/manshal_khatri/authmefirebase/MainActivity.kt
+++ app/src/main/java/com/manshal_khatri/authmefirebase/MainActivity.kt
@@ -2,10 +2,13 @@ package com.manshal_khatri.authmefirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
